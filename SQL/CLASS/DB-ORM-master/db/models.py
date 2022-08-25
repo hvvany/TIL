14933d2ae92a7be8cb86afafbfba485ccd5c86b5
hvvany <kjunhwan1998@gmail.com>
2022-08-26 08:46:35 +0900

    add 0825 class

--- SQL/CLASS/DB-ORM-master/db/models.py
+++ SQL/CLASS/DB-ORM-master/db/models.py
@@ -9,3 +9,10 @@ class Director(models.Model):
 class Genre(models.Model):
     title = models.TextField()
 
+class Movie(models.Model):
+    director = models.ForeignKey(Director,on_delete=models.CASCADE)
+    genre = models.ForeignKey(Genre,on_delete=models.CASCADE)
+    title = models.TextField()
+    opening_date = models.DateField()
+    running_time = models.IntegerField()
+    screening = models.BooleanField()
