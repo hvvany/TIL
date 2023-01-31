ee22242a737f8caf77f58e630a1cbb183e3ea6e5
junhwan <kjunhwan1998@gmail.com>
2023-01-31 20:19:19 +0900

    fix:solve

--- Algorithm/Baekjoon/code_folder/1446.py
+++ Algorithm/Baekjoon/code_folder/1446.py
@@ -1,3 +1,13 @@
-n = int(input())
-for i in range(1e9//6 + 1):
-    (n - 1)//6
+INF = 1e9
+
+n, total = map(int,input().split())
+node_set = {0,total}
+for _ in range(n):
+    s, e, l = map(int,input().split())
+    node_set.add(s)
+    node_set.add(e)
+node_cnt = len(node_set)
+graph = [[] for i in range(node_cnt+1)]
+visited = [False] * (n + 1)
+distance = [INF] * (n + 1)
+
