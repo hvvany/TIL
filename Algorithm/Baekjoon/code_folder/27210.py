e68b705159a95af5394dddf4a19262fed24bda90
junhwan <kjunhwan1998@gmail.com>
2023-03-21 20:19:19 +0900

    algo

--- Algorithm/Baekjoon/code_folder/27210.py
+++ Algorithm/Baekjoon/code_folder/27210.py
@@ -5,7 +5,8 @@ input = sys.stdin.readline
 n = int(input())
 inform_lst = list(map(int,input().split()))
 memory = [n,-n]  # [최소값, 최대값]
-
+print(f'inform_lst : {inform_lst}')
+lst = []
 깨달음 = 0
 for i in range(n):
     if inform_lst[i] == 1:
@@ -16,6 +17,9 @@ for i in range(n):
         memory[0] = 깨달음
     if 깨달음 > memory[1]:
         memory[1] = 깨달음
+    lst.append(깨달음)
+
+print(f'sum_lst : {lst}')
 if memory[0] > 0:
     memory[0] = 0
 if memory[1] < 0:
